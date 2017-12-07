e14adec5ea001def43662397dfd809c9b96e4982
JerryYin <jerryyin@JerryYins-MacBook-Pro.local>
2017-12-07 15:37:16 +0800

    class 3 [Work with View Controllers] done!

--- PhotoFilters/ViewController.swift
+++ PhotoFilters/ViewController.swift
@@ -8,16 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
     //MARK: properties
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var mealNameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         //handle the text field's user input through delegate callbacks
         nameTextField.delegate = self
-        
     }
     
     //MARK: UITextFieldDelegate   --> override method to hadle user input
@@ -36,6 +36,46 @@ class ViewController: UIViewController, UITextFieldDelegate {
     @IBAction func setDefaultLabelText(_ sender: UIButton) {
         mealNameLabel.text = "Default Text"
     }
+    
+    //select image from library
+    @IBAction func selectImageFromLibrary(_ sender: UITapGestureRecognizer) {
+        //hide the keyboard
+        nameTextField.resignFirstResponder()
+        
+        // UIImagePickerController is a view controller that lets a user pick media from their photo library.
+        let imagePickerController = UIImagePickerController()
+        
+        // only allow photos to be picked, not taken
+        imagePickerController.sourceType = .photoLibrary
+        
+        //make sure ViewController is notified when the user picks an image
+        imagePickerController.delegate = self
+        
+        present(imagePickerController, animated: true, completion: nil)
+    }
+    
+    
+    
+    //MARK: UIImagePickerControllerDelegate
+    //called when user canceled iamge picker
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        // Dismiss the picker if the user canceled.
+        dismiss(animated: true, completion: nil)
+    }
+    
+    // gets called when a user selects a photo.
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        // The info dictionary may contain multiple representations of the image. You want to use the original.
+        guard let selectedImage = info[UIImagePickerControllerOriginalImage] as?UIImage
+    else {
+            fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+        }
+        //set image view
+        photoImageView.image = selectedImage
+        //Dissmiss picker
+        dismiss(animated: true, completion: nil)
+    }
+    
     
 
 }
